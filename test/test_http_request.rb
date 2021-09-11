0bb8f6c13dc2f079fa39adb5e556e625c201d01d
Ryo Takahashi <ryogiftjp@gmail.com>
2021-09-11 17:50:11 +0900

    fix syntax

--- test/test_http_request.rb
+++ test/test_http_request.rb
@@ -4,20 +4,16 @@ require "./lib/http_request"
 # httpbin.orgへのリクエストテスト
 class TestHttpRequest < Minitest::Test
   def test_nil_argument_error
-    assert_raises(ArgumentError) do
-      http_request = HttpRequest.new
-    end
+    assert_raises(ArgumentError) { HttpRequest.new }
   end
 
   def test_blank_argument_error
-    assert_raises(ArgumentError) do
-      http_request = HttpRequest.new ""
-    end
+    assert_raises(ArgumentError) { HttpRequest.new "" }
   end
 
   def test_execute
     http_request = HttpRequest.new("https://httpbin.org/uuid")
     response = http_request.execute
-    assert response[:uuid].size > 0
+    assert response[:uuid].size.positive?
   end
 end
